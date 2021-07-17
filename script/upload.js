a3086a94c782b18dd5e74ad8930a8296a750bf98
lijunhong <lijunhong@bytedance.com>
2021-07-17 15:27:56 +0800

    feat: 迁移到ghpages

--- script/upload.js
+++ script/upload.js
@@ -1,27 +1,14 @@
-const path = require('path');
-const { parsed: env } = require('dotenv-flow').config();
-const FtpDeploy = require('ftp-deploy');
-const ftpDeploy = new FtpDeploy();
+const ghpages = require('gh-pages');
 
-const config = {
-  host: env.HOST_DOMIAN,
-  user: env.HOST_USERNAME,
-  password: env.HOST_PASSWORD,
-  port: 21,
-  localRoot: path.resolve('./dist'),
-  remoteRoot: '/web',
-  include: ['*', '**/*'], // this would upload everything except dot files
-  // e.g. exclude sourcemaps, and ALL files in node_modules (including dot files)
-  exclude: ['**/*.map'],
-  // delete ALL existing files at destination before uploading, if true
-  deleteRemote: true,
-  // Passive mode is forced (EPSV command is not sent)
-  forcePasv: true,
-  // use sftp or ftp
-  sftp: false,
-};
-
-ftpDeploy
-  .deploy(config)
-  .then(res => console.log('finished:', res))
-  .catch(err => console.log(err));
+ghpages.publish(
+  'dist',
+  {
+    user: {
+      name: 'TIS-OMiddle',
+      email: 'tis.omiddle@gmail.com',
+    },
+  },
+  function (err) {
+    console.log('err: ', err);
+  },
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,12 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 import vitePluginImport from 'vite-plugin-babel-import';
 import copy from 'rollup-plugin-copy';
 import { resolve } from 'path';
+console.log(process.env);
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:
+    process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/tis-omiddle/ehtranslate-web@gh-pages' : '/',
   build: {
     commonjsOptions: {
       transformMixedEsModules: true,
